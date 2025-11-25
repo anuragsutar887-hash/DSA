7af31253909c35ed40e0610e74e4bfc7472955cf
anuragsutar887-hash <anuragsutar887@gmail.com>
2025-11-25 10:45:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (61.82%) - LeetSync

---
+++ 3321-type-of-triangle/type-of-triangle.c
@@ -0,0 +1,13 @@
+char* triangleType(int* nums, int numsSize) {
+    int a=nums[0], b=nums[1], c=nums[2];
+    if(a+b<=c || b+c<=a || c+a<=b){
+        return "none";
+    }
+    if(a==b && b==c){
+        return "equilateral";
+    }
+    if(a==b || b==c || c==a){
+        return "isosceles";
+    }
+    return "scalene";
+}
